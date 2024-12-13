3bb2c3e02fcecf1fb0657580013333be4b07697e
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-12-13 13:24:09 +0530

    Add initial backend setup with environment configuration and API structure

---
+++ backend/src/database/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connect = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        ); 
+        console.log(
+            `Database :: connect :: success :: ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.error("Error :: Database :: connect :: error", error);
+        process.exit(1);
+    }
+};
+
+export default connect;
